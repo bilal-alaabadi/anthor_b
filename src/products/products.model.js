16a339cc5855d69ed07951a17029310e258a9c98
bilal-alaabadi <mdbilalnizwa@gmail.com>
2026-09-12 21:27:46 +0400

    Update products.model.js

--- src/products/products.model.js
+++ src/products/products.model.js
@@ -2,19 +2,19 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    category: { type: String, required: true },
-    size: { type: String }, // إضافة حقل الحجم
+    name:        { type: String, required: true },
+    category:    { type: String, required: true },
     description: { type: String, required: true },
-    price: { type: Number, required: true }, // السعر العادي لجميع المنتجات
-    image: { type: [String], required: true },
-    oldPrice: { type: Number },
-    rating: { type: Number, default: 0 },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+    price:       { type: Number, required: true },
+    image:       { type: [String], required: true },
+    oldPrice:    { type: Number },
+    rating:      { type: Number, default: 0 },
+    author:      { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    inStock:     { type: Boolean, default: true },
+    stock:       { type: Number, required: true, min: 0 },
   },
   { timestamps: true }
 );
 
 const Products = mongoose.model("Product", ProductSchema);
-
 module.exports = Products;
